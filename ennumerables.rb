7b0b51533c6174e8269f8ba93ed485b05071ee5e
Kevin Cotrina <kcotrinam92@gmail.com>
2020-09-15 23:35:29 -0500

    reset dev

--- ennumerables.rb
+++ ennumerables.rb
@@ -1,22 +1,3 @@
 module Enumerable
-  def my_each
-    if self.class == Array
-      for i in (0..self.length - 1)
-        yield(self[i])
-      end
-    else
-      keys = self.keys
-      values = self.values
-      key_value_arr = []
-      for i in (0..self.length - 1) 
-        temp_arr = []
-        temp_arr[0] = keys[i]
-        temp_arr[1] = values[i]
-        key_value_arr << temp_arr
-      end
-      for i in (0..key_value_arr.length - 1)
-        yield(key_value_arr[i])
-      end
-    end
-  end
+
 end
