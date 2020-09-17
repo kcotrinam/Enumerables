b82455aec27fa79800dc4a1aad598d4ec6dee1a2
Mohamed Dawood <devmohamedawood@gmail.com>
2020-09-17 14:12:35 +0200

    fix linter errors

--- ennumerables.rb
+++ ennumerables.rb
@@ -1,27 +1,15 @@
-# rubocop: disable Metrics/ModuleLength
-# rubocop: disable Metrics/PerceivedComplexity, Metrics/CyclomaticComplexity
-module
- Enumerable
-
+module Enumerable
   def my_each
     return to_enum(:my_each) unless block_given?
 
     arr = self if self.class == Array
-
     arr = to_a if self.class == Range
-
     arr = arr.flatten if self.class == Hash
-
     counter = 0
-
     while counter < arr.length
-
       yield(arr[counter])
-
       counter += 1
-
     end
-
     self
   end
 
@@ -29,21 +17,13 @@ module
     return to_enum(:my_each_with_index) unless block_given?
 
     arr = self if self.class == Array
-
     arr = to_a if self.class == Range
-
     arr = arr.flatten if self.class == Hash
-
     counter = 0
-
     while counter < arr.length
-
       yield(arr[counter], counter)
-
       counter += 1
-
     end
-
     self
   end
 
@@ -51,111 +31,67 @@ module
     return to_enum(:my_select) unless block_given?
 
     new_arr = []
-
     my_each { |item| new_arr << item if yield(item) }
-
     new_arr
   end
 
   def my_all?(arg = nil)
     if block_given?
-
       my_each { |item| return false if yield(item) == false }
-
       return true
-
     elsif arg.nil?
-
       my_each { |n| return false if n.nil? || n == false }
-
     elsif !arg.nil? && (arg.is_a? Class)
-
       my_each { |n| return false if n.class != arg }
-
     elsif !arg.nil? && arg.class == Regexp
-
       my_each { |n| return false unless arg.match(n) }
-
     else
-
       my_each { |n| return false if n != arg }
-
     end
-
     true
   end
 
   def my_any?(arg = nil)
     if block_given?
-
       my_each { |item| return true if yield(item) }
-
       false
-
     elsif arg.nil?
-
       my_each { |n| return true if n }
-
     elsif !arg.nil? && (arg.is_a? Class)
-
       my_each { |n| return true if n.class == arg }
-
     elsif !arg.nil? && arg.class == Regexp
-
       my_each { |n| return true if arg.match(n) }
-
     else
-
       my_each { |n| return true if n == arg }
-
     end
-
     false
   end
 
   def my_none?(arg = nil)
     if !block_given? && arg.nil?
-
       my_each { |n| return false if n }
-
       return true
-
     end
-
     if !block_given? && !arg.nil?
-
       if arg.is_a?(Class)
-
         my_each { |n| return false if n.class == arg }
-
         return true
-
       end
 
       if arg.class == Regexp
-
         my_each { |n| return false if arg.match(n) }
-
         return true
-
       end
-
       my_each { |n| return false if n == arg }
-
       return true
-
     end
-
     my_any? { |item| return false if yield(item) }
-
     true
   end
 
   def my_count(num = nil)
     arr = self.class == Array ? self : to_a
-
     return arr.length unless block_given? || num
-
     return arr.my_select { |item| item == num }.length if num
 
     arr.my_select { |item| yield(item) }.length
@@ -165,59 +101,38 @@ module
     return to_enum unless block_given? || proc
 
     new_arr = []
-
     if proc
-
       my_each { |item| new_arr << proc.call(item) }
-
     else
-
       my_each { |item| new_arr << yield(item) }
-
     end
-
     new_arr
   end
 
   def my_inject(num = nil, sym = nil)
     if block_given?
-
       accumulator = num
-
       my_each do |item|
         accumulator = accumulator.nil? ? item : yield(accumulator, item)
       end
-
       accumulator
-
     elsif !num.nil? && (num.is_a?(Symbol) || num.is_a?(String))
-
       accumulator = nil
-
       my_each do |item|
         accumulator = accumulator.nil? ? item : accumulator.send(num, item)
       end
-
       accumulator
-
     elsif !sym.nil? && (sym.is_a?(Symbol) || sym.is_a?(String))
-
       accumulator = num
-
       my_each do |item|
         accumulator = accumulator.nil? ? item : accumulator.send(sym, item)
       end
-
       accumulator
-
     end
   end
 end
 
-# rubocop: enable
-
 public
-
 def multiply_els(arr)
   arr.my_inject { |result, element| result * element }
 end
